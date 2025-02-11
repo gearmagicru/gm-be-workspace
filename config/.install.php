ce5fd868bc2d148ffb589e266d0ebc19d4efc6b4
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-02-11 11:41:05 +0300

    Файл конфигурации установки модуля

--- config/.install.php
+++ config/.install.php
@@ -2,6 +2,8 @@
 /**
  * Этот файл является частью модуля веб-приложения GearMagic.
  * 
+ * Файл конфигурации установки модуля.
+ * 
  * @link https://gearmagic.ru
  * @copyright Copyright (c) 2015 Веб-студия GearMagic
  * @license https://gearmagic.ru/license/
