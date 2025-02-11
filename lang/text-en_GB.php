6bfab2f728244e1edfb3cdfa724ac836647fa546
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-02-11 11:37:40 +0300

    Файлы локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Этот файл является частью модуля веб-приложения GearMagic.
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Workspace',
+    '{description}' => 'Workspace Control Panel',
+    '{permissions}' => [],
+
+    // Bootslide / Widgets
+    'Workspace' => 'Workspace',
+    'Panel control' => 'Panel control',
+    'Logout' => 'Logout',
+    'Account' => 'Account',
+    'Back' => 'Back',
+    'Settings' => 'Configuration',
+    'Common' => 'Common',
+    'Help' => 'Help',
+
+    // Index
+    'widget' => 'widget',
+
+    // Welcome
+    'Welcome to "{0}"' => 'Welcome to "{0}"',
+
+    // WidgetsTree
+    'view all tree nodes' => 'view all tree nodes (modules) in the navigation panel',
+];
---
+++ lang/text-ru_RU.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Этот файл является частью модуля веб-приложения GearMagic.
+ * 
+ * Пакет русской локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Рабочее пространство',
+    '{description}' => 'Рабочее пространство Панели управления',
+    '{permissions}' => [],
+
+    // Bootslide / Widgets
+    'Workspace' => 'Рабочее пространство',
+    'Panel control' => 'Панель управления',
+    'Logout' => 'Выйти',
+    'Account' => 'Аккаунт',
+    'Back' => 'Назад',
+    'Settings' => 'Конфигурация',
+    'Common' => 'Сведения',
+    'Help' => 'Помощь',
+
+    // Index
+    'widget' => 'виджет',
+
+    // Welcome
+    'Welcome to "{0}"' => 'Добро пожаловать в "{0}"',
+
+    // WidgetsTree
+    'view all tree nodes' => 'просмотр всех узлов дерева (модулей) в панеле навигации',
+];
